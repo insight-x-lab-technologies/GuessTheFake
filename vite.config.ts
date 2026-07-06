3547401f12736f4e94ae0a0a8b5cf2b7ae16edca
matiello <matiello@gmail.com>
2026-07-06 20:06:14 -0300

    PWA icons

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,25 @@ export default defineConfig({
             src: 'assets/icons/icon.svg',
             sizes: 'any',
             type: 'image/svg+xml',
-            purpose: 'any maskable'
+            purpose: 'any'
+          },
+          {
+            src: 'assets/icons/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'assets/icons/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'assets/icons/maskable-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
           }
         ]
       },
